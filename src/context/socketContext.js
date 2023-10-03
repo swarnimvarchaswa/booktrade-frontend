cafc84cbc0e5554de5134bf5d82e0bdef24d1f00
swarnimvarchaswa <2021031178@mmmut.ac.in>
2023-10-03 12:09:20 +0530

    add loading bar

--- src/context/socketContext.js
+++ src/context/socketContext.js
@@ -1,91 +1,6 @@
-// import React, { createContext, useContext, useState, useEffect } from "react";
-// import io from "socket.io-client";
-
-// const SocketContext = createContext();
-
-// export const useSocket = () => {
-//   return useContext(SocketContext);
-// };
-
-// export const SocketProvider = ({ children }) => {
-//   const [socket, setSocket] = useState(null);
-//   const [token, setToken] = useState(null);
-
-//   useEffect(() => {
-//     const token = localStorage.getItem("jwt");
-//     if (!token) {
-
-//     }
-//   }, []);
-
-//   const fetchData = async () => {
-//     try {
-//       const fetchUser = async () => {
-//         const response = await fetch(
-//           "https://booktrade-api.onrender.com/loginuser",
-//           {
-//             headers: {
-//               Authorization: "Bearer " + localStorage.getItem("jwt"),
-//             },
-//           }
-//         );
-//         const userId = await response.text();
-//         return userId;
-//       };
-
-//       const newUserId = await fetchUser();
-
-//       // Check if the token has changed
-//       if (newUserId !== token) {
-//         // Disconnect existing socket
-//         if (socket) {
-//           socket.disconnect();
-//         }
-
-//         // Set up a new socket connection
-//         const ENDPOINT = "http://localhost:5000";
-//         const newSocket = io(ENDPOINT);
-
-//         // Set up socket authentication
-//         newSocket.auth = {
-//           token: newUserId.toString(),
-//         };
-//         newSocket.connect();
-
-//         // Set the new socket and token in state
-//         setSocket(newSocket);
-//         setToken(newUserId);
-//       }
-//     } catch (error) {
-//       console.error("Error fetching data:", error);
-//     }
-//   };
-
-//   useEffect(() => {
-//     // Initial fetch when component mounts
-//     fetchData();
-
-//     // Set up interval to check for token changes every 10 seconds
-//     const intervalId = setInterval(fetchData, 10000);
-
-//     // Cleanup interval on component unmount
-//     return () => clearInterval(intervalId);
-//   }, []); // Dependencies array should be empty since we only want to run this once on mount
-
-//   if (!socket) {
-//     // Return loading or placeholder component if the socket is not available
-//     return <div></div>;
-//   }
-
-//   return (
-//     <SocketContext.Provider value={socket}>{children}</SocketContext.Provider>
-//   );
-// };
-
-// export default SocketProvider;
-
 import React, { createContext, useContext, useState, useEffect } from "react";
 import io from "socket.io-client";
+import Navbar from "../Components/Navbar";
 
 const SocketContext = createContext();
 
@@ -185,8 +100,19 @@ export const SocketProvider = ({ children }) => {
   if (localStorage.getItem("jwt")) {
     if (!socketReady) {
       // Return loading or placeholder component if the socket is not available
-      return <div> </div>;
-    } else {
+      return (
+        <div className="max-w-md mx-auto px-12 my-4 container flex flex-col"> 
+          {/* <Navbar /> */}
+          <h1 className="text-gray-800 text-5xl font-r font-normal py-10 text-center">
+        <span className="text-purple-700">book</span>trade
+      </h1>
+          {/* <img
+            className="max-w-md w-full mx-auto"
+            src="https://res.cloudinary.com/booktrade/image/upload/v1695586780/Circle_Loader_nkgtip.gif"
+            alt="Loading"
+          /> */}
+        </div>
+      );
     }
   }
 
