7610fb1bc208c6153154b62b7e298c8d2abbd3f4
swarnimvarchaswa <2021031178@mmmut.ac.in>
2024-08-10 17:48:17 +0530

    resolving modal

--- src/Components/Modal.jsx
+++ src/Components/Modal.jsx
@@ -1,3 +1,48 @@
+// import React from "react";
+// import { useSocket } from "../context/socketContext";
+
+// export default function Modal({ setModalOpen }) {
+//   const {socket, setIsNewMessage, isNewMessage} = useSocket();
+
+//   return (
+//     <div
+//       className="fixed z-80 flex items-center justify-center w-screen h-screen bg-black bg-opacity-40 "
+//       onClick={() => setModalOpen(false)}
+//     >
+//       <div className="relative z-10 w-72  py-2 text-center bg-white rounded-xl">
+//         <div className="py-5 font-medium font-r tracking-wide">
+//           <p>Log out of your account?</p>
+//         </div>
+//         <hr className="" />
+//         <div className="grid justify-stretch">
+//           <button
+//             className="logOutBtn py-3 font-medium font-r tracking-wider text-red-500 "
+//             onClick={() => {
+//               setModalOpen(false);
+//               localStorage.clear();
+//               if (socket) {
+//                 socket.disconnect();
+//               }
+//             }}
+//           >
+//             Log out
+//           </button>
+//         </div>
+//         <hr className="" />
+//         <div className="grid justify-stretch">
+//           <button
+//             className="cancelbtn py-3 font-r tracking-wider"
+//             onClick={() => setModalOpen(false)}
+//           >
+//             Cancel
+//           </button>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
+
+
 import React from "react";
 import { useSocket } from "../context/socketContext";
 
@@ -6,7 +51,7 @@ export default function Modal({ setModalOpen }) {
 
   return (
     <div
-      className="fixed z-70 flex items-center justify-center w-screen h-screen bg-black bg-opacity-40 "
+      className="absolute top-0 left-0 z-80 flex items-center justify-center w-screen h-screen bg-black bg-opacity-40 "
       onClick={() => setModalOpen(false)}
     >
       <div className="relative z-10 w-72  py-2 text-center bg-white rounded-xl">
